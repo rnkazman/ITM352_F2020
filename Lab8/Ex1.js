d4cd26c8c8f3d02b9d155d087b0b03298500cc68
rnkazman <kazman@hawaii.edu>
2020-10-11 17:33:07 -1000

    Update Ex1.js

--- Lab8/Ex1.js
+++ Lab8/Ex1.js
@@ -1,17 +1,6 @@
-<<<<<<< HEAD
 age = 48;
 ageCount = 1;
 while (age > ageCount) {
     console.log(`Age= ${ageCount}`);
     ageCount++;
 }
-=======
-var number = 1; // Counter for age
-var myAge = 8; // My age
-
-console.log("Silly counting program for Ex1");
-while (number <= myAge) {
-    console.log("Age=" + number);
-    number++;
-}
->>>>>>> 4cde92a15be7e93ffb93d6f1002f4fa019d989bf
