8992bfba730c231aa86c47e14bf127803b5842ec
rnkazman <kazman@hawaii.edu>
2020-10-20 22:04:24 -1000

    Lab10 stuff

--- HowManyPizza.js
+++ HowManyPizza.js
@@ -1,5 +1,5 @@
 const howManyPizza = require("how-many-pizza");
-for(teamSize = 1; teamSize<=12; teamSize++)
-{
+
+for (teamSize = 1; teamSize <= 12; teamSize++) {
     console.log("You need %s pizzas for %d people", howManyPizza(teamSize), teamSize);
 }
