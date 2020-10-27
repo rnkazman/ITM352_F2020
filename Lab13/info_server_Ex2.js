3d5e58ac2138563ca8f5652e4599d6d36f46c9dc
rnkazman <kazman@hawaii.edu>
2020-10-26 16:39:01 -1000

    asdf

---
+++ Lab13/info_server_Ex2.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var myParser = require("body-parser");
+
+app.use(express.static('./public'));
+app.use(myParser.urlencoded({ extended: true }));
+
+app.all('*', function (request, response, next) {
+    console.log(request.method + ' to ' + request.path);
+    next();
+});
+
+app.post("/process_form", function (request, response) {
+    console.log("Got POST");
+    let POST = request.body;
+    response.send(POST); 
+});
+
+app.listen(8080, () => console.log(`listening on port 8080`));
